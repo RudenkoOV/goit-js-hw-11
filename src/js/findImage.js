b1a438f96d8b014a0c1000745b30e395462fc348
RudenkoOV <kpaxx@ukr.net>
2023-02-19 18:49:12 +0200

    9

--- src/js/findImage.js
+++ src/js/findImage.js
@@ -1,12 +1,12 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 export default async function findImage(key, text, imageType, orientation, safesearch, pageNumber) {
-    try {
-        const response = await fetch(`https://pixabay.com/api/?key=${key}&q=${text}&image_type=${imageType}&orientation=${orientation}&safesearch=${safesearch}&page=${pageNumber}&per_page=12`);
+
+        const response = await fetch(`https://pixabay.com/api/?key=${key}&q=${text}&image_type=${imageType}&orientation=${orientation}&safesearch=${safesearch}&page=${pageNumber}&per_page=40`);
         const searchResult = await response.json();
-        if (searchResult.hits.length == 0) {
+        if (searchResult.hits.length == 0 && pageNumber === 1) {
             Notify.failure('Sorry, there are no images matching your search query. Please try again.');
-        } else if (pageNumber === 1) { Notify.info(`Hooray! We found ${searchResult.totalHits} images.`); }
+        } else if (searchResult.hits.length == 0 && pageNumber > 1) {Notify.failure(`We're sorry, but you've reached the end of search results.`)}
+            else if (pageNumber === 1) { Notify.info(`Hooray! We found ${searchResult.totalHits} images.`); } 
         return searchResult;
-    }
-    catch {Notify.failure('Sorry, there are no images matching your search query. Please try again.')}
+
 }
